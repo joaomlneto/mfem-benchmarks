4f7ce1a9ca9788fa98da5045a4e3f96da8137e87
João Neto <joao@neto.pt>
2018-09-30 20:15:57 +0100

    Update check-omp.sh

--- scripts/check-omp.sh
+++ scripts/check-omp.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-cpubindings=("0-7" "0-15" "0-23" "0-31" "0-39" "0-47")
+#cpubindings=("0-7" "0-15" "0-23" "0-31" "0-39" "0-47")
+cpubindings=("0-47" "0-39" "0-31" "0-23" "0-15" "0-7")
 
 run_cmd () {
-  echo ${red}$1${reset}
-  $1
+  echo ${red}$@${reset}
+  $@ >/dev/null
 }
 
 echo executable and args: "$@"
@@ -15,6 +16,6 @@ reset=`tput sgr0`
 for (( i=0; i<${#cpubindings[@]}; i++ ));
 do
   echo -e "${red}#CPUs: ${numcpus[$i]} \tCPUs bound: ${cpubindings[$i]}${reset}"
-  run_cmd "time numactl --physcpubind=${cpubindings[$i]} $@"
-  run_cmd "time numactl --physcpubind=${cpubindings[$i]} --interleave=all $@"
+  run_cmd "/usr/bin/time -f%e numactl --physcpubind=${cpubindings[$i]} $@"
+  run_cmd "/usr/bin/time -f%e numactl --physcpubind=${cpubindings[$i]} --interleave=all $@"
 done
